2d4c12277cd226013b2d17c9516c1890dcd6cc68
John Bartos <jbartos7@gmail.com>
2015-08-11 22:49:27 -0400

    Add request, call twitch channel_stream endpoint

--- server/api/audio-stream/audio-stream.controller.js
+++ server/api/audio-stream/audio-stream.controller.js
@@ -1,6 +1,19 @@
 'use strict'
 
-exports.get = function(req,res)
-{
-  res.json({ message: 'yo' });   
+var request = require('request');
+
+var options = {
+  uri : "https://api.twitch.tv/api/channels/arteezy/access_token",
+  method: 'GET',
+  headers: {'user-agent': 'node.js'}
+}
+
+exports.get = function(req,res) {
+  console.log('Getting access token')
+  request(options, function (error, response, body) {
+    console.log('Response from Twitch ' + response.statusCode);
+    if (!error && response.statusCode == 200) {
+      res.json(body);
+    }
+  })
 };
