44f51d3331d962a67f26db8446c15c58613ce48a
John Bartos <jbartos7@gmail.com>
2015-08-25 22:31:47 -0400

    Debug mode player

--- client/components/stream-service/stream-service.js
+++ client/components/stream-service/stream-service.js
@@ -1,16 +1,18 @@
 angular.module('radioFreeTwitch')
 .service('streamService', function () {
 
-  this.setup = function(container) {
+  this.setup = function(container, stream) {
     flowplayer(container, {
       clip: {
+        live: true,
         sources: [
           {
             type: 'video/webm',
             src: 'http://stream.flowplayer.org/drive.webm'
           }
         ]
-      }
+      },
+      debug: true,
     });
   }
 });
