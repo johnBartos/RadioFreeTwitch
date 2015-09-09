2bbae1b3dc1c2142974ad6478de86cbb4f4d89ec
John Bartos <jbartos7@gmail.com>
2015-09-08 22:22:02 -0400

    Bind html to child object of $scope

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,33 +1,34 @@
 angular.module('radioFreeTwitch')
   .controller('MainController', function($scope, $http, streamService) {
 
-    $scope.isCollapsed = false;
-    $scope.streamer = "";
+    $scope.stream = {};
+    $scope.stream.isCollapsed = false;
+    $scope.stream.streamer = "";
+    $scope.stream.volume = 0;
+    $scope.stream.player = null;
 
     function getStream (streamer) {
-      var stream = $http.get('api/audio-stream/' + streamer + '.m3u8')
+      var streamUri = $http.get('api/audio-stream/' + streamer + '.m3u8')
         .then(function(result) {
             console.log('proxy url is ' + result.data);
             return result.data;
         }, function(error) {
             console.log('ERROR ' + error);
         });
-      return stream;
+      return streamUri;
     }
 
-    $scope.play = function () {
-        getStream($scope.streamer).then(function (stream){
-        console.log('stream is ' + stream);
-
-        $scope.player = streamService.setup(
-          angular.element(document.getElementById('player')),
-          streamService.buildClip(stream));
-      });
-
+    $scope.stream.play = function () {
+        getStream($scope.stream.streamer).then(function (stream){
+          console.log('stream is ' + stream);
+          $scope.stream.player = streamService.setup(
+            angular.element(document.getElementById('player')),
+            streamService.buildClip(stream));
+        });
     }
 
-    $scope.togglePlayer = function () {
-        var player = $scope.player;
+    $scope.stream.togglePlayer = function () {
+        var player = $scope.stream.player;
         console.log('player: ' + player);
 
         if(player.paused) {
@@ -38,10 +39,9 @@ angular.module('radioFreeTwitch')
         }
     }
 
-    $scope.refreshPlayer = function () {
-      getStream($scope.streamer).then(function (stream){
-        var player = $scope.player;
-
+    $scope.stream.refreshPlayer = function () {
+      getStream($scope.stream.streamer).then(function (stream){
+        var player = $scope.stream.player;
         player.load(streamService.buildClip(stream));
       });
     }
