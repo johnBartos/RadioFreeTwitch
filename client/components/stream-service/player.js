602e90e4e7d1e042e2cb7f7d0fee8576eddbd0ac
John Bartos <jbartos7@gmail.com>
2015-11-24 22:21:02 -0500

    Working on creating object to represent stream

--- client/components/stream-service/player.js
+++ client/components/stream-service/player.js
@@ -1,30 +1,22 @@
-function Player() {
+var player = function player(manifestUri) {
 
-  this.buildClip = function (stream) {
-    return {
+  var buildClip = function (streamName) {
+      return  {
         sources: [
           {
             type: 'application/x-mpegurl',
             src: stream
           }
-        ]
+      ]
     };
   };
 
-}
+  var setup = function(playerContainer, manifest) {
+    var manifestClip = buildClip(manifest);
+    var player = flowplayer(playerContainer, {
+      autoplay: true,
+      clip: manifestClip
+    });
+  };
 
-
-Player.prototype.play = function(stream) {
-
-};
-
-Player.prototype.setup = function(playerContainer, stream) {
-  var clip = buildClip(stream);
-
-  var player = flowplayer(playerContainer, {
-    autoplay: true,
-    clip: streamClip
-  });
-
-  this.player = player;
 };
