b7ecdc1c88460dab6623e6f35a168bba749da116
John Bartos <jbartos7@gmail.com>
2015-11-29 22:23:04 -0500

    More cleanup

--- server/api/chunk-proxy/chunk-proxy.controller.js
+++ server/api/chunk-proxy/chunk-proxy.controller.js
@@ -3,42 +3,40 @@
 var request= require('request');
 var rp = require('request-promise');
 
-
 exports.get = function(req, res) {
   console.log('proxy get chunk');
 
-  var chunk = decodeURIComponent(req.params.chunk);
-  var buf = [];
-  var gotError = false;
-
   var getChunk = function(chunk) {
+    var gotError = false;
+    var buf = [];
     var options = getChunkOptions(chunk);
+
     return request(options)
-    .on('error', function(reason) {
-      gotError = true;
-      this.end();
-      console.log('error encountered, closed stream ' + reason);
-    })
-    .on('data',function(streamChunk) {
-      console.log('got data');
-      buf.push(streamChunk);
-    })
-    .on('end', function(body) {
-      console.log('stream ended');
+      .on('error', function(reason) {
+        gotError = true;
+        this.end();
+        console.log('error encountered, closed stream ' + reason);
+      })
+      .on('data',function(streamChunk) {
+        console.log('got data');
+        buf.push(streamChunk);
+      })
+      .on('end', function(body) {
+        console.log('stream ended');
 
-      if(gotError) {
-        res.status(404).end();
-        return;
-      }
+        if(gotError) {
+          res.status(404).end();
+          return;
+        }
 
-      this.end();
-      var newRes = Buffer.concat(buf);
-      res.status(200).send(newRes);
-    });
+        this.end();
+        var newRes = Buffer.concat(buf);
+        res.status(200).send(newRes);
+      });
   };
 
+  var chunk = decodeURIComponent(req.params.chunk);
   getChunk(chunk);
-  buf = [];
 };
 
 function getChunkOptions(chunk) {
