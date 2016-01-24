5e95d3e0a4acb52abf68e5e50a53833a54177616
John Bartos <jbartos7@gmail.com>
2016-01-23 19:19:48 -0500

    Styling

--- client/js/components/Header.jsx
+++ client/js/components/Header.jsx
@@ -1,17 +1,20 @@
 const React = require('react');
 
 const headerStyle = {
-  backgroundColor: 'rgba(40, 0, 215, 0.9)',
+  // backgroundColor: 'rgba(255, 255, 255, 1.0)',
+  // backgroundColor: 'white',
   borderRadius: '0',
-  color: '#FFF'
+  color: '#000',
+  marginBottom: '5em'
+  // boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
 };
 
 const Header = React.createClass({
   render: function renderNavbar() {
     return (
-      <div className="jumbotron dotted" style={headerStyle}>
-        <h1 className="display-4">Stream <hr style= {{ backgroundColor: 'white' }}/></h1>
-        <h1 className="display-4" style={{ paddingLeft: '1em' }}>Rad.io</h1>
+      <div className="col-xs-2" style={headerStyle}>
+        <h1 className="display-4">stream <hr style= {{ backgroundColor: 'rgba(0, 0, 0, 1.0)' }}/></h1>
+        <h1 className="display-4" style={{ paddingLeft: '1em' }}>rad.io</h1>
       </div>
     );
   }
--- client/js/components/Searchbar.jsx
+++ client/js/components/Searchbar.jsx
@@ -1,6 +1,11 @@
 const React = require('react');
 const ReactPropTypes = React.PropTypes;
 
+const style = {
+  color: 'black',
+  border: '1px solid black'
+};
+
 const Searchbar = React.createClass({
   getInitialState: () => {
     return {
@@ -10,11 +15,11 @@ const Searchbar = React.createClass({
   render: function renderSearchbar() {
     return (
       <div className="row search-bar">
-        <div className="col-xs-offset-2 col-xs-8">
+        <div className="col-xs-offset-3 col-xs-6">
          <div className="input-group">
-           <input type="text" className="form-control" name="search-bar" value={this.state.stream} onChange={this._onChange}></input>
+           <input type="text" className="form-control" name="search-bar" value={this.state.stream} onChange={this._onChange} style={style}></input>
            <span className="input-group-btn">
-             <button className="btn btn-secondary" type="submit" onClick={this.onPress}>Go!</button>
+             <button className="btn btn-secondary" type="submit" onClick={this.onPress} style={style}>Go!</button>
            </span>
          </div>
        </div>
